d466b9602df7c5cdd702c6305ec6676fe709b69b
Ana Carolina Guimarães <a.carolina1903@gmail.com>
2022-05-01 21:42:45 -0300

    refatorei o código para parar de repetir a digitação do menu no código

--- src/main/java/Index.java
+++ src/main/java/Index.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class Index {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int novaDecisao = 0;
+        int decisao = 0;
         System.out.println("Bem-vindx ao facilitor de seus calculos e decisões!\n ");
-        System.out.println("MENU");
-        System.out.println("1 - Créditos " +
-                "\n2 - Decisão: Podemos ir à praia?" +
-                "\n3 - Cálculo de descontos " +
-                "\n4 - Cálculo de IMC" +
-                "\n5 - Cálculo de tarifa da conta de luz para grupo normal" +
-                "\n6 - Cálculo de tarifa de água e esgoto" +
-                "\n7 - Encerrar sessão.");
-        System.out.println("Digite um número da opção desejada: ");
-        int decisao = sc.nextInt();
+
 
         do {
+            System.out.println("\nMENU");
+            System.out.println("1 - Créditos " +
+                    "\n2 - Decisão: Podemos ir à praia?" +
+                    "\n3 - Cálculo de descontos " +
+                    "\n4 - Cálculo de IMC" +
+                    "\n5 - Cálculo de tarifa da conta de luz para grupo normal" +
+                    "\n6 - Cálculo de tarifa de água e esgoto" +
+                    "\n7 - Encerrar sessão.");
+            System.out.println("Digite um número da opção desejada: ");
+            decisao = sc.nextInt();
+
             switch (decisao) {
                 case 1:
                     System.out.println("------------CRÉDITOS------------" +
@@ -26,18 +28,7 @@ public class Index {
                             "\nCleiton Ribeiro da Silva;" +
                             "\nDaniel Augusto Magalhães França;" +
                             "\nJenifer Mirela Sousa Alves.");
-                    System.out.println("\n");
-                    System.out.println("\nMENU");
-                    System.out.println("1 - Créditos " +
-                            "\n2 - Decisão: Podemos ir à praia?" +
-                            "\n3 - Cálculo de descontos " +
-                            "\n4 - Cálculo de IMC" +
-                            "\n5 - Cálculo de tarifa da conta de luz para grupo normal" +
-                            "\n6 - Cálculo de tarifa de água e esgoto" +
-                            "\n7 - Encerrar sessão.");
-                    System.out.println("Digite um número da opção desejada: ");
-                    novaDecisao = sc.nextInt();
-                    decisao = novaDecisao;
+                    break;
                 case 2:
                 case 3:
                 case 4:
@@ -47,20 +38,8 @@ public class Index {
                     System.out.println("Digite a quantidade de kWh consumidos: ");
                     double kwhConsumidos = sc.nextDouble();
                     double calculoDeTarifaLuz = kwhConsumidos *  precoEnergia + kwhConsumidos * precoUsoSistemaDistribuicao;
-
                     System.out.printf("Valor calculado com sucesso! Sua tarifa da conta de luz deu: R$%.2f", calculoDeTarifaLuz);
-                    System.out.println("\n");
-                    System.out.println("\nMENU");
-                    System.out.println("1 - Créditos " +
-                            "\n2 - Decisão: Podemos ir à praia?" +
-                            "\n3 - Cálculo de descontos " +
-                            "\n4 - Cálculo de IMC" +
-                            "\n5 - Cálculo de tarifa da conta de luz para grupo normal" +
-                            "\n6 - Cálculo de tarifa de água e esgoto" +
-                            "\n7 - Encerrar sessão.");
-                    System.out.println("Digite um número da opção desejada: ");
-                    novaDecisao = sc.nextInt();
-                    decisao = novaDecisao;
+                    break;
                 case 6:
                 case 7:
                     System.out.println("------------CRÉDITOS------------" +
